56b586c17feaca3875b6e28b2b1217f1e10ab0d7
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-07-18 02:57:33 +0530

    added models

--- auctions/models.py
+++ auctions/models.py
@@ -6,3 +6,19 @@ class User(AbstractUser):
     pass
 
 
+class Items(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='item')
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(balnk=False)
+    minimum = models.IntegerField(blank=False)
+
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bid')
+    amount = models.IntegerField(blank=False)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField(blank=False)
